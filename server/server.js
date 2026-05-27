93bc78ed959c3ddc72d6f17ad8ceddcc8ce5726e
Akash <akash@gmail.com>
2026-05-27 11:15:46 +0530

    Fixed backend CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+
 require('dotenv').config()
 
 const facilityRoutes = require('./routes/facilityRoutes')
@@ -10,7 +11,14 @@ const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  })
+)
+
 app.use(express.json())
 
 app.use('/api/facilities', facilityRoutes)
@@ -19,14 +27,18 @@ app.use('/api/bookings', bookingRoutes)
 app.use('/api/auth', authRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.log(err))
+.then(() => {
+  console.log('MongoDB Connected')
+})
+.catch((err) => {
+  console.log(err)
+})
 
 app.get('/', (req, res) => {
   res.send('Sports Booking API Running')
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
